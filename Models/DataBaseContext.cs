25998290e6dcfd563c916387a123f7b19f6ecefc
louy755 <lumberjack1695.com>
2021-03-13 19:17:19 -0600

    inital commit

---
+++ Models/DataBaseContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIAPPAjaxCRUD.Models
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+
+}
